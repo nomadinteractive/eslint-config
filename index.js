06e1b002bcc9efce087b59ac51a81b0249075f14
Sercan Virlan <sercan@nomadinteractive.co>
2020-12-27 22:48:20 +0300

    Update index.js
    Removed flowtype rules.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = {
     "parser": "babel-eslint",
     "extends": [
-        "airbnb",
-        "plugin:flowtype/recommended"
+        "airbnb"
     ],
     "env": {
         "browser": true,
@@ -27,32 +26,11 @@ module.exports = {
         "react/jsx-indent-props": [2, "tab"],
         "react/jsx-no-bind": 0,
         "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
-        "react/prefer-stateless-function": 0,
-		// FlowJS rules
-        "flowtype/boolean-style": [2, "boolean"],
-        "flowtype/define-flow-type": 1,
-        "flowtype/delimiter-dangle": [2, "never"],
-        "flowtype/generic-spacing": [2, "never"],
-        "flowtype/no-primitive-constructor-types": 2,
-        "flowtype/no-types-missing-file-annotation": 2,
-        "flowtype/no-weak-types": "warn",
-        "flowtype/object-type-delimiter": [2, "comma"],
-        "flowtype/require-parameter-type": 2,
-        "flowtype/require-return-type": [2, "always", { "annotateUndefined": "always", "excludeMatching": [ "constructor" ]    }],
-        "flowtype/require-valid-file-annotation": 2,
-        "flowtype/semi": ["error", "never"],
-        "flowtype/space-after-type-colon": [2, "always"],
-        "flowtype/space-before-generic-bracket": [2, "never"],
-        "flowtype/space-before-type-colon": [2, "never"],
-        "flowtype/type-id-match": [2, "^([A-Z][a-z0-9]+)+Type$"],
-        "flowtype/union-intersection-spacing": [2, "always"],
-        "flowtype/use-flow-type": 1,
-        "flowtype/valid-syntax": 1,
+        "react/prefer-stateless-function": 0
     },
     "plugins": [
         "import",
         "react",
-        "flowtype",
         "unicorn"
     ],
     "settings": {
@@ -60,9 +38,6 @@ module.exports = {
             "node": {
                 "extensions": [".js", ".ios.js", ".android.js"]
             }
-        },
-        "flowtype": {
-            "onlyFilesWithFlowAnnotation": true
         }
     }
 }
